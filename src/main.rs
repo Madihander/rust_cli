e8a5fe611459d60ccfe63b2bfafa4faf208646c3
madihander <79453431+Madihander@users.noreply.github.com>
2023-09-11 09:42:14 +0600

    Cli can read file and find line with specific pattern

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::env;
 use clap::Parser;
 
 #[derive(Parser)]
@@ -13,5 +12,15 @@ fn main() {
     println!("It is rust cli \n");
 
     let args = CLI::parse();
-    
+    // Get all content of file from the path
+    let content = std::fs::read_to_string(&args.path).expect("Could not read file");
+    // Iterating by every line in file
+    for line in content.lines() {
+        // find line that contain pattern
+        if line.contains(&args.pattern){
+            println!("{}",line);
+        }
+    }
+    //println!("Pattern: {}",args.pattern);
+    //println!("Pattern: {:?}",args.path);
 }
