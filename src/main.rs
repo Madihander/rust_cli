dcae49b3598e83b2f55119cafec30a1da1458e4a
madihander <79453431+Madihander@users.noreply.github.com>
2023-09-11 09:07:55 +0600

    Add pattern and path variables that will use for realise grep functional

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::io;
+use std::env;
 fn main() {
     println!("Hello user!");
     println!("It is rust cli \n");
+
+    let pattern = env::args().nth(1).expect("no pattern given");
+    let path = env::args().nth(2).expect("no path given");
+
+    //let mut cmd = String::new();
+    //println!("Enter command: ");
     
-    let mut cmd = String::new();
-    println!("Enter command: ");
-    
-    let _ = io::stdin().read_line(&mut cmd); 
-    println!("Command: {}",cmd);
+    //let _ = io::stdin().read_line(&mut cmd); 
+    //println!("Command: {}",cmd);
 
 }
