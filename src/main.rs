aa2e52328befcd16d4b256c28f8579e73f22a686
madihander <79453431+Madihander@users.noreply.github.com>
2023-09-11 08:42:58 +0600

    Add minimal interface - get command and print command name

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
-    println!("Train git")
+    println!("Hello user!");
+    println!("It is rust cli \n");
+    let mut cmd = String::new();
+    println!("Enter command: ");
+    let _ = io::stdin().read_line(&mut cmd); 
+    println!("Command: {}",cmd)
 }
