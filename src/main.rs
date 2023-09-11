88ea2504c057f06281e5afeec1d9b2d928c30a0e
madihander <79453431+Madihander@users.noreply.github.com>
2023-09-11 08:53:15 +0600

    Again commit new changes

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::io;
 fn main() {
     println!("Hello user!");
     println!("It is rust cli \n");
+    
     let mut cmd = String::new();
     println!("Enter command: ");
+    
     let _ = io::stdin().read_line(&mut cmd); 
     println!("Command: {}",cmd);
-    
+
 }
