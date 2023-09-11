399eb80b5cb67de92126ccc79da8935eb86551a1
madihander <79453431+Madihander@users.noreply.github.com>
2023-09-11 09:23:08 +0600

    Add library clap to simplyfy

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-use std::io;
 use std::env;
+use clap::Parser;
+
+#[derive(Parser)]
 struct CLI{
+    // The pattern to loor for
     pattern: String,
+    // The path to the file to read
     path: std::path::PathBuf,
 }
 fn main() {
     println!("Hello user!");
     println!("It is rust cli \n");
 
-    let pattern = env::args().nth(1).expect("no pattern given");
-    let path = env::args().nth(2).expect("no path given");
-
-    //let mut cmd = String::new();
-    //println!("Enter command: ");
+    let args = CLI::parse();
     
-    //let _ = io::stdin().read_line(&mut cmd); 
-    //println!("Command: {}",cmd);
-
 }
